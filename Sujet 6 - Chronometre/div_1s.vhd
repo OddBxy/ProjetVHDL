68e62c98c4305fe34216bb8f28d08c29ac338cea
Epiiiix <118908919+Epiiiix@users.noreply.github.com>
2024-04-12 18:51:54 +0200

    Reset chaque seconde

--- Sujet 6 - Chronometre/div_1s.vhd
+++ Sujet 6 - Chronometre/div_1s.vhd
@@ -14,27 +14,17 @@ end div_1s;
 architecture compteur of div_1s is
 
   signal compteur_1s : std_logic_vector(27 downto 0) := (others => '0');
-   signal greenFlag : std_logic := '0';
   begin 
     process(CLK, RST)
       begin
         if(RST = '1') then
           compteur_1s <= (others => '0');
         elsif(CLK'event and CLK = '1') then 
---          if(compteur_1s>=x"5F5E0FF") then
---            compteur_1s <= (others => '0');
---          else
---                compteur_1s <= compteur_1s + "0000001";
---        end if;
---          end if;
           compteur_1s <= compteur_1s + '1';
-          if(compteur_1s = "011000000000000000000000000") then
-            greenFlag <= '1';
-          else 
-            greenFlag <= '0';
+          if(compteur_1s = x"5F5E0FF") then
+            compteur_1s <= (others => '0');
           end if;
         end if;
       end process;
-    CLK_1S <= greenFlag;
---    CLK_1S <= '1' when compteur_1s=x"5F5E0FF" else '0';
+    CLK_1S <= '1' when compteur_1s = x"5F5E0FF" else '0';
 end compteur;
