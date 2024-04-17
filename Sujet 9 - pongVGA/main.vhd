e7fb26442fbbd3569d6d129c6b3818cd47a7224c
Epiiiix <118908919+Epiiiix@users.noreply.github.com>
2024-04-17 10:42:23 +0200

    Ajout du field

--- Sujet 9 - pongVGA/main.vhd
+++ Sujet 9 - pongVGA/main.vhd
@@ -37,6 +37,17 @@ component vga_controller_640_60 is
   );
 end component;
 
+component field is 
+  port(
+    div_25MHZ : in std_logic;
+    hcount : in std_logic_vector(10 downto 0);
+    vcount : in std_logic_vector(10 downto 0);
+    blank : in std_logic;
+    RED : out std_logic_vector(3 downto 0);
+    GREEN : out std_logic_vector(3 downto 0);
+    BLUE : out std_logic_vector(3 downto 0)
+  );
+end component;   
 
 signal CLK_OUT : std_logic := '0';
 signal hcount : std_logic_vector(10 downto 0) := (others => '0');
@@ -47,6 +58,6 @@ begin
   
   DUT0 : div_25MHZ port map(CLK, RST, CLK_OUT);
   DUT1 : vga_controller_640_60 port map(RST, CLK_OUT, HS, VS, hcount, vcount, blank);
+  DUT2 : field port map(CLK_OUT, hcount, vcount, blank, RED, GREEN, BLUE);
   
 end behaviour;
-  
