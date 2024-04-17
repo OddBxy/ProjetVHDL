fb28d38c81b3204156c185bc5efcb550f6cb446d
OddBxy <118909861+OddBxy@users.noreply.github.com>
2024-04-17 10:58:14 +0200

    Update field.vhd

--- Sujet 9 - pongVGA/field.vhd
+++ Sujet 9 - pongVGA/field.vhd
@@ -15,28 +15,75 @@ entity field is
     );
   end field;
 
-  architecture display of field is
-    begin 
+architecture display of field is
+  
+component racket is
+  port (
+    CLK : in std_logic;
+    RST : in std_logic;
+    UP : in std_logic;
+    DOWN : in std_logic;
+    player : in std_logic;    --si 0 -> raquette gauche, s1 1 -> raquette droite
+    
+    
+    x1 : out std_logic_vector(10 downto 0) := "00000000000";    
+    y1 : out std_logic_vector(10 downto 0) := "00011010001";
+    x2 : out std_logic_vector(10 downto 0) := "00000001000";
+    y2 : out std_logic_vector(10 downto 0) := "00100010000"
+  );
+end component;
+
+signal RST : std_logic := '0';
+signal UP : std_logic := '0';
+signal DOWN : std_logic := '0';
+signal player0 : std_logic := '0';
+signal player1 : std_logic := '1';
+
+signal x1 : std_logic_vector(10 downto 0);
+signal x2 : std_logic_vector(10 downto 0);
+signal y1 : std_logic_vector(10 downto 0);
+signal y2 : std_logic_vector(10 downto 0);
+
+signal x3 : std_logic_vector(10 downto 0);
+signal x4 : std_logic_vector(10 downto 0);
+signal y3 : std_logic_vector(10 downto 0);
+signal y4 : std_logic_vector(10 downto 0);
+
+begin 
+  
+  RACKET1 : racket port map(div_25MHZ, RST, UP, DOWN, player0, x1, x2, y1, y2);
+  RACKET2 : racket port map(div_25MHZ, RST, UP, DOWN, player1, x3, x4, y3, y4);
       
-      process(div_25MHZ, blank)
-        begin 
-          if(rising_edge(div_25MHZ)) then
-            if(blank = '0') then
-                if(hcount >= 318 AND hcount <= 322) then
-                    RED <= "1111";
-                    GREEN <= "1111";
-                    BLUE <= "1111";
-                else
-                    RED <= "0000";
-                    GREEN <= "0000";
-                    BLUE <= "0000"; 
-                end if; 
-            elsif(blank = '1') then
-                RED <= "0000";
-                GREEN <= "0000";
-                BLUE <= "0000";
-            end if;
+  process(div_25MHZ, blank)
+    begin 
+      if(rising_edge(div_25MHZ)) then
+        if(blank = '0') then
+          if(hcount >= 318 AND hcount <= 322) then
+            RED <= "1111";
+            GREEN <= "1111";
+            BLUE <= "1111";
+          else
+            RED <= "0000";
+            GREEN <= "0000";
+            BLUE <= "0000"; 
           end if;
-        end process;
+          
+          if(hcount >= x1 and hcount <= x2 and vcount >= y1 and vcount <= y2) then
+            RED <= "1111";
+            GREEN <= "1111";
+            BLUE <= "1111";
+          elsif(hcount >= x1 and hcount <= x2 and vcount >= y1 and vcount <= y2) then
+            RED <= "1111";
+            GREEN <= "1111";
+            BLUE <= "1111";
+          end if;         
+           
+        elsif(blank = '1') then
+          RED <= "0000";
+          GREEN <= "0000";
+          BLUE <= "0000";
+        end if;
+      end if;
+  end process;
         
-    end display;
+end display;
